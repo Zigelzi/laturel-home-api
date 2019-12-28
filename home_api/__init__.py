78b70a48e8f68660dd73f7c956a1c599c1329beb
zigelzi <miika.a.savela@gmail.com>
2019-12-28 19:16:33 +0200

    Added configuration file. Added first housing association route for POST/GET requests.

--- home_api/__init__.py
+++ home_api/__init__.py
@@ -2,7 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+from home_api.config import Config, DevConfig
+
 app = Flask(__name__)
+app.config.from_object(DevConfig)
 # Enable Cross Origin Resource Sharing (CORS)
 CORS(app, resources={r'/*':{'origins':'*'}}) 
 
--- home_api/routes.py
+++ home_api/routes.py
@@ -1,8 +1,34 @@
-from flask import jsonify
+from flask import jsonify, request
 
 from home_api import app
 from home_api.config import Config, DevConfig
 
-@app.route('/home')
-def home():
-    return jsonify({'taloyhtiö':'hakulinpuisto'})
+@app.route('/housing_associations', methods=['GET', 'POST'])
+def all_housing_associations():
+    
+    response_object = {'status': 'success'}
+
+    housing_associations = [
+        {
+          'name': "Hakulinpuisto",
+          'businessId': "1234567-8",
+          'street': "Hakulintie",
+          'streetNumber': 43,
+          'postalCode': "08500",
+          'city': "Lohja"
+        }
+    ]
+    if request.method == 'POST':
+        post_data = request.get_json()
+        housing_associations.append({
+            'name': post_data.get('name'),
+            'businessId': post_data.get('businessId'),
+            'street': post_data.get('street'),
+            'streetNumber': post_data.get('streetNumber'),
+            'postalCode': post_data.get('postalCode'),
+            'city': post_data.get('city')
+        })
+        response_object['message'] = 'Housing association added!'
+    else:
+        response_object['housingAssociations'] = housing_associations
+    return jsonify(response_object)
