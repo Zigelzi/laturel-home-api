319f61d92494eb547fcaa56be2061b03f2568ea5
zigelzi <miika.a.savela@gmail.com>
2020-01-03 19:07:21 +0200

    Added Building and Apartment models

--- home_api/models.py
+++ home_api/models.py
@@ -21,9 +21,43 @@ class HousingAssociation(db.Model):
     city = db.Column(db.String(100))
     created = db.Column(db.DateTime, default=datetime.utcnow)
 
+    # Foreign keys and relationships
+    buildings = db.relationship('Building', backref='housing_association', lazy='dynamic')
+
     def __repr__(self):
         return f'<HousingAssociation {self.name} | {self.business_id} | {self.street} {self.street_number} | {self.postal_code} {self.city}>'
 
+class Building(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    building_name = db.Column(db.String(10)) # Building name, usually letter
+    
+    # Foreign keys and relationships
+    housing_association_id = db.Column(db.Integer, db.ForeignKey('housing_association.id'))
+    apartments = db.relationship('Apartment', backref='building', lazy='dynamic')
+    # TODO: Add repairs relationship
+
+    def __repr__(self):
+        return f'<Building {self.building_name}>'
+
+class Apartment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    apartment_number = db.Column(db.Integer)
+
+    # Foreign keys
+    building_id = db.Column(db.Integer, db.ForeignKey('building.id'))
+
+    def __repr__(self):
+        return f'<Apartment {self.apartment_number}>'
+
+# Marshmallow serialization schemas
 class HousingAssociationSchema(ma.ModelSchema):
     class Meta:
         model = HousingAssociation
+
+class BuildingSchema(ma.ModelSchema):
+    class Meta:
+        model = Building
+
+class ApartmentSchema(ma.ModelSchema):
+    class Meta:
+        model = Apartment
